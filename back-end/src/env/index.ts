b2194a290dc5c40bb718b00184eae356a2e19e9a
João Manoel <joaomanoeldev@gmail.com>
2024-05-03 18:51:22 -0300

    chore: create envSchema to validate environment variables

---
+++ back-end/src/env/index.ts
@@ -0,0 +1,16 @@
+import "dotenv/config"
+import { z } from "zod"
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+  PORT: z.coerce.number().default(3333)
+})
+
+const response = envSchema.safeParse(process.env)
+
+if (!response.success) {
+  console.error("Invalid environment variables.", response.error.format())
+  throw new Error("Invalid environment variables.")
+}
+
+export const env = response.data
