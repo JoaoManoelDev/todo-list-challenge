048a9c9637040a0e397a16e8c3ae1a6075fb5aa5
João Manoel <joaomanoeldev@gmail.com>
2024-05-03 19:43:54 -0300

    feat: create authenticate use case and tests

---
+++ back-end/src/use-cases/authenticate.spec.ts
@@ -0,0 +1,53 @@
+import { beforeEach, describe, expect, it } from "vitest"
+
+import { passwordHash } from "@/lib/hash"
+import {
+  InMemoryUsersRepository
+} from "@/repositories/in-memory/in-memory-users-repository"
+import { AuthenticateUseCase } from "./authenticate"
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error"
+
+let usersRepository: InMemoryUsersRepository
+let sut: AuthenticateUseCase
+
+describe("Authenticate Use Case", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new AuthenticateUseCase(usersRepository)
+  })
+  
+  it("should be able to authenticate", async () => {
+    await usersRepository.create({
+      name: "John Doe",
+      email: "johndoe@email.com",
+      password_hash: await passwordHash({ password: "123456" })
+    })
+
+    const { user } = await sut.execute({
+      email: "johndoe@email.com",
+      password: "123456"
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it("should not be able to authenticate with wrong email", async () => {
+    await expect(() => sut.execute({
+      email: "johndoe@email.com",
+      password: "123456"
+    })).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+
+  it("should not be able to authenticate with wrong password", async () => {
+    await usersRepository.create({
+      name: "John Doe",
+      email: "johndoe@email.com",
+      password_hash: await passwordHash({ password: "123456" })
+    })
+
+    await expect(() => sut.execute({
+      email: "johndoe@email.com",
+      password: "654321"
+    })).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+})
---
+++ back-end/src/use-cases/authenticate.ts
@@ -0,0 +1,42 @@
+import { User } from "@prisma/client"
+
+import { UsersRepository } from "@/repositories/users-repository"
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error"
+import { comparePassword } from "@/lib/hash"
+
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateUseCaseResponse {
+  user: User,
+}
+
+export class AuthenticateUseCase {
+  constructor(
+    private usersRepository: UsersRepository
+  ) {}
+
+  async execute({
+    email,
+    password
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await comparePassword({
+      password,
+      hash: user.password_hash
+    })
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return { user }
+  }
+}
