127c87b4b12a20a71defae358f93d3b77897b581
João Manoel <joaomanoeldev@gmail.com>
2024-05-04 15:12:11 -0300

    feat: add token to sign in and create ensure authenticated middleware

--- back-end/src/http/controllers/authenticate.ts
+++ back-end/src/http/controllers/authenticate.ts
@@ -1,8 +1,10 @@
 import { NextFunction, Request, Response } from "express"
 import { z } from "zod"
+import { sign } from "jsonwebtoken"
 
 import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate-use-case"
 import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-error"
+import { env } from "@/env"
 
 export class AuthenticateController {
   async handler(request: Request, response: Response, next: NextFunction) {
@@ -16,7 +18,14 @@ export class AuthenticateController {
 
       const authenticateUseCase = makeAuthenticateUseCase()
   
-      await authenticateUseCase.execute({ email, password })
+      const { user } = await authenticateUseCase.execute({ email, password })
+
+      const token = sign({ name: user.name }, env.JWT_SECRET, {
+        subject: user.id,
+        expiresIn: "3h"
+      })
+
+      return response.status(200).json({ token })
     } catch (error) {
       if (error instanceof InvalidCredentialsError) {
         return response.status(409).send({ message: error.message })
@@ -24,7 +33,5 @@ export class AuthenticateController {
 
       return next(error)
     }
-
-    return response.status(200).end()
   }
 }
---
+++ back-end/src/middlewares/ensure-authenticated.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from "express"
+import { verify } from "jsonwebtoken"
+
+import { env } from "@/env"
+import { InvalidTokenError } from "@/errors/invalid-token-error"
+
+interface Payload {
+  sub: string
+  name: string
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  try {
+    const authHeader = request.headers.authorization
+
+    if (!authHeader) {
+      throw new InvalidTokenError()
+    }
+
+    const [_bearer, token] = authHeader.split(" ")
+
+    const {
+      sub: userId,
+      name: userName
+    } = verify(token, env.JWT_SECRET) as Payload
+
+    request.user = {
+      id: userId,
+      name: userName
+    }
+
+    next()
+  } catch(error) {
+    if (error instanceof InvalidTokenError) {
+      return response.status(401).send({ message: error.message })
+    }
+  }
+}
