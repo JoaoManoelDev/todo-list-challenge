37084fb2a555e4f153c4cadf5c13955ef4738846
João Manoel <joaomanoeldev@gmail.com>
2024-05-06 15:57:36 -0300

    chore: add cors

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 
 import { routes } from "@/http/routes"
 import { errorHandler } from "@/middlewares/error"
@@ -6,6 +7,8 @@ import { errorHandler } from "@/middlewares/error"
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 app.use(routes)
 
