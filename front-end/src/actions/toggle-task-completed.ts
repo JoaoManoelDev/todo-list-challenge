3016929ed4e4d8797ed038ec11c44632ea877633
João Manoel <joaomanoeldev@gmail.com>
2024-05-14 16:25:35 -0300

    feat: create toggleTaskCompleted action

---
+++ front-end/src/actions/toggle-task-completed.ts
@@ -0,0 +1,30 @@
+"use server"
+
+import { revalidatePath } from "next/cache"
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
+
+import { api } from "@/data/api"
+
+export const toggleTaskCompleted = async (taskId: string) => {
+  const response = await api(`/task/toggle-completed/${taskId}`, {
+    method: "PATCH",
+  })
+
+  console.log("[ACTION TOGGLE TASK COMPLETED TASK]", response)
+
+  if (response.status === 200) revalidatePath("/")
+
+  if (response.status === 409) {
+    return { error: "Tarefa não encontrada." }
+  }
+  
+  if (response.status === 401) {
+    cookies().delete("@todo:token-auth")
+    redirect("/sign-in")
+  }
+
+  return { error: "Algo deu errado, tente novamente mais tarde." }
+
+}
+
