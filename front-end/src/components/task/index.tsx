7da60e4ed9d8fd158307cb6cc3466fe3fe784b7e
João Manoel <joaomanoeldev@gmail.com>
2024-05-14 23:24:31 -0300

    refactor: add DeleteModal and ToggleButton component

--- front-end/src/components/task/index.tsx
+++ front-end/src/components/task/index.tsx
@@ -1,23 +1,11 @@
-"use client"
-
-import { toast } from "sonner"
-import { AxiosError } from "axios"
-
 import { cn } from "@/lib/utils"
 import { Icons } from "@/components/icons"
-import { Task as TaskType, useTaskContext } from "@/contexts/task"
 import { UpdateTaskForm } from "./update-task-form"
 import {
   AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
   AlertDialogTrigger
 } from "@/components/ui/alert-dialog"
+import { DeleteModal } from "./delete-modal"
 import {
   Dialog,
   DialogContent,
@@ -25,58 +13,27 @@ import {
   DialogTitle,
   DialogTrigger
 } from "@/components/ui/dialog"
+import { ToggleButton } from "./toggle-button"
 
-
-export const Task = ({ id, title, is_completed }: TaskType) => {
-  const { toggleTaskCompleted, deleteTask, logOut } = useTaskContext()
-
-  const handleToggleTaskCompleted = async () => {
-    try {
-      await toggleTaskCompleted(id)
-    } catch (error) {
-      if (error instanceof AxiosError) {
-        if (error.response?.status === 409) toast.error("Tarefa não encontrada.")
-        if (error.response?.status === 401) logOut()
-      } else {
-        toast.error("Erro ao cadastrar usuário. Por favor, tente novamente mais tarde.")
-      }
-    }
+interface ITask {
+  task: {
+    id: string
+    title: string
+    is_completed: boolean
   }
+}
 
-  const handleDeleteTask = async () => {
-    try {
-      await deleteTask(id)
-    } catch (error) {
-      if (error instanceof AxiosError) {
-        if (error.response?.status === 409) toast.error("Tarefa não encontrada.")
-        if (error.response?.status === 401) logOut()
-      } else {
-        toast.error("Erro ao cadastrar usuário. Por favor, tente novamente mais tarde.")
-      }
-    }
-  }
-
+export const Task = ({ task }: ITask) => {
   return (
     <div
       className={cn(
-        "flex border p-4 rounded-lg mt-5 gap-2", is_completed ?
+        "flex border p-4 rounded-lg mt-5 gap-2", task.is_completed ?
         "line-through" : ""
       )}
     >
-      <button
-        className=""
-        onClick={() => handleToggleTaskCompleted()}
-      >
-        {
-          is_completed ?
-          <Icons.checkCircle className="w-5 h-5" /> :
-          <div className="border-2 rounded-full w-5 h-5 border-primary" />
-        }
-      </button>
+      <ToggleButton task={task} />
 
-      <p className="">
-        {title}
-      </p>
+      <p>{task.title}</p>
 
       <div className="ml-auto flex gap-2">
         <AlertDialog>
@@ -84,21 +41,7 @@ export const Task = ({ id, title, is_completed }: TaskType) => {
             <Icons.trash className="w-5 h-5 text-red-500" />
           </AlertDialogTrigger>
 
-          <AlertDialogContent>
-
-            <AlertDialogHeader>
-              <AlertDialogTitle>Tem certeza disso</AlertDialogTitle>
-              <AlertDialogDescription>
-                Ao clicar em continuar você excluirá permanente esta tarefa.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction onClick={() => handleDeleteTask()}>Continuar</AlertDialogAction>
-            </AlertDialogFooter>
-
-          </AlertDialogContent>
+          <DeleteModal task={task} />
         </AlertDialog>
 
         <Dialog>
@@ -111,13 +54,7 @@ export const Task = ({ id, title, is_completed }: TaskType) => {
               <DialogTitle>Editar Tarefa</DialogTitle>
             </DialogHeader>
 
-            <UpdateTaskForm
-              task={{
-                id,
-                title,
-                is_completed
-              }}
-            /> 
+            <UpdateTaskForm task={task} /> 
           </DialogContent>
         </Dialog>
       </div>
